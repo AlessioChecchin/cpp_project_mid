eab0989bd28a69eafd9e54a87d38d9d64709c229
jacopix <jacopo@studiotoniolo.eu>
2023-11-28 14:42:41 +0100

    Commented test_book.cpp

--- src/test/test_book.cpp
+++ src/test/test_book.cpp
@@ -8,6 +8,10 @@ namespace test
 
 void test_book_constructor()
 {
+
+	/**
+	 * Testing default constructor
+	*/
 	prj::Book e {};
 	check(e.get_isbn() == "0000000000000");
 	check(e.get_title() == "");
@@ -16,9 +20,25 @@ void test_book_constructor()
 	check(e.get_state() == prj::Book::State::Available);
 	check(e.has_copyright() == false);
 
-	try { e.get_copyright_date(); check(false); }
-	catch(const std::logic_error& err) { check(true); }
 
+	/**
+	 * The default constructor must not have a date. 
+	 * If the object 'e' has a date, an exception must be thrown
+	*/
+	try 
+	{ 
+		e.get_copyright_date(); 
+		check(false); 
+	}
+	catch(const std::logic_error& err) 
+	{ 
+		check(true); 
+	}
+
+
+	/**
+	 * Testing second constructor (name, surname, isbn)
+	*/
 	prj::Book a{"Name", "Surname", "Title", "isbn-00001111"};
 	check(a.get_isbn() == "isbn-00001111");
 	check(a.get_author_name() == "Name");
@@ -27,6 +47,9 @@ void test_book_constructor()
 	check(a.get_state() == prj::Book::State::Available);
 	check(a.has_copyright() == false);
 
+	/**
+	 * Testing third constructor (name, surname, isbn, date, state)
+	*/
 	prj::Book b{"Name2", "Surname2", "Title2", "isbn-22221111", prj::Date(29, 9, 2003), prj::Book::State::OnLoan };
 	check(b.get_isbn() == "isbn-22221111");
 	check(b.get_author_name() == "Name2");
@@ -34,16 +57,6 @@ void test_book_constructor()
 	check(b.get_title() == "Title2");
 	check(b.get_state() == prj::Book::State::OnLoan);
 	check(b.has_copyright() == true);
-
-	try
-	{
-		prj::Date dt = b.get_copyright_date();
-
-		check(dt.get_day() == 29);
-		check(dt.get_month() == 9);
-		check(dt.get_year() == 2003);
-	}
-	catch(const std::logic_error& err) { check(false); }
 }
 
 } // test
